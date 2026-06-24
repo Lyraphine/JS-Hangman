36d26730d1778551622f2d6dfa8442677c2ce4c8
Lyraphine <lyraphine@gmail.com>
2026-06-24 05:01:14 -0700

    All game element code integrated and functioning

--- scripts/final-project.js
+++ scripts/final-project.js
@@ -1,27 +1,41 @@
 // Author: Lydia Asselstine
 
+const alphabet         = "abcdefghijklmnopqrstuvwxyz".split("");
+const $image           = $("#image");
+const $keyboard        = $("#keyboard");
+const $incorrect       = $("#incorrect");
+const $word            = $("#word");
+const $hint            = $("#hint");
+const $hintButton      = $("#hint-btn");
+const $reset           = $("#reset");
+const instructionsText = $("#instructions p").html();
+const wrongGuessMax    = 6;
+let   wrongGuesses     = 0;
+
+
 // ****************
 // *** KEYBOARD ***
 // ****************
-const alphabet  = "abcdefghijklmnopqrstuvwxyz".split("");
-const $keyboard = $("#keyboard");
 
 // Create buttons for each letter of the alphabet
 // Add a click event listener to each
-alphabet.forEach(function(letter) 
+function createKeyboard()
 {
-    const $button = $("<button>");
-
-    $button.text(letter);
-    $button.addClass("keyboard-key");
-
-    $button.click(function() 
+    alphabet.forEach(function(letter) 
     {
-        disableGuess($button)
-    });
+        const $button = $("<button>");
 
-    $keyboard.append($button);
-});
+        $button.text(letter);
+        $button.addClass("keyboard-key");
+
+        $button.click(function() 
+        {
+            handleGuess($button, letter);
+        });
+
+        $keyboard.append($button);
+    });
+}
 
 // Keyboard input events
 $(document).keydown(function(event) 
@@ -40,11 +54,12 @@ $(document).keyup(function(event)
     const letter  = event.key.toLowerCase();
     const $button = findKey(letter);
 
-    if($button)
+    if($button && !$button.prop("disabled"))
     {
-        disableGuess($button);
-        $button.removeClass("active"); 
+        handleGuess($button, letter); 
     }
+    
+    $button.removeClass("active");
 });
 
 // Finds the matching button for the letter that was pressed
@@ -54,11 +69,11 @@ function findKey(letter)
 
     $keyboard.children().each(function() 
     {
-        const $button = $(this);
+        const $key = $(this);
 
-        if($button.text() === letter)
+        if($key.text() === letter)
         {
-            $match = $button;
+            $match = $key;
         }
     });
 
@@ -66,7 +81,7 @@ function findKey(letter)
 }
 
 // Disables the button
-function disableGuess($button)
+function disableKey($button)
 {
     $button.prop("disabled", true);
 }
@@ -75,41 +90,197 @@ function disableGuess($button)
 // ************
 // *** DATA ***
 // ************
-const $word = $("#word");
-const $hint = $("#hint");
 
-fetch("../data/words.json")
-    .then(function(response)
+// Fetch random JSON data from file and display it
+function fetchData()
+{  
+    fetch("../data/words.json")
+        .then(function(response)
+        {
+            if(response.ok)
+            {
+                return response.json();
+            }
+            else
+            {
+                throw new Error("Failed to fetch words");
+            }
+        })
+        .then(function(data)
+        {
+            let randomIndex  = Math.floor(Math.random() * data.length);
+            let currentWord  = data[randomIndex];
+            let wordLetters  = currentWord.word.split("");
+            let hint         = currentWord.hint;
+
+            // Display each letter of the new word
+            wordLetters.forEach(function(letter)
+            {
+                const $letterTile = $("<span>");
+
+                $letterTile.data("letter", letter);
+                $letterTile.text("_");
+                $letterTile.addClass("letter-tile");
+
+                $word.append($letterTile);
+            });
+
+            // Display the hint
+            $hint.text(hint);
+        })
+        .catch(function(error)
+        {
+            $word.text(error);
+        });
+}
+
+
+// ******************
+// *** START GAME ***
+// ******************
+
+// Click listener for Start button
+$reset.click(function()
+{
+    startGame();
+});
+
+// Start game by resetting all game elements, 
+// showing all relevant elements, 
+// and fetching new data
+function startGame()
+{
+    wrongGuesses = 0; 
+
+    $keyboard.empty();
+    $word.empty();
+    $hint.empty();
+    $hint.hide();
+
+    createKeyboard();
+    fetchData();
+
+    $keyboard.show();
+    $incorrect.show();
+    $word.show();
+    $hintButton.show();
+
+    $reset.text("Reset Game");
+    $incorrect.text("Wrong guesses: " + wrongGuesses);
+    $("#instructions p").html(instructionsText);
+}
+
+
+// ***************
+// *** GUESSES ***
+// ***************
+
+// Click listener for Hint button
+$hintButton.click(function()
+{
+    $hint.show();
+    $hintButton.hide();
+})
+
+// Display the tile letter when there is a matching letter guess
+// Returns true for match, false otherwise
+function revealLetter(letter)
+{   
+    let match = false;
+
+    $word.children().each(function()
     {
-        if(response.ok)
+        const $letterTile = $(this);
+        
+        if ($letterTile.data("letter") === letter)
         {
-            return response.json();
+            $letterTile.text(letter.toUpperCase());
+            match = true;
         }
-        else
-        {
-            throw new Error("Failed to fetch words");
-        }
-    })
-    .then(function(data)
+    });
+
+    return match;
+}
+
+// Checking for win or lose conditions
+function checkGameState()
+{
+    if(wrongGuesses > wrongGuessMax)
     {
-        let randomIndex = Math.floor(Math.random() * data.length);
-        let currentWord = data[randomIndex];
-        let wordLetters = currentWord.word.split("");
-        let hint        = currentWord.hint;
+        loseCondition();
+    }
+    else
+    {
+        let win = true;
 
-        wordLetters.forEach(function(letter)
+        $word.children().each(function()
         {
-            const $letterTile = $("<span>");
-
-            $letterTile.text("_");
-            $letterTile.addClass("letter-tile");
-
-            $word.append($letterTile);
+            const $letterTile = $(this);
+        
+            if ($letterTile.text() === "_")
+            {
+                win = false;
+            }
         });
 
-        $hint.text(hint);
-    })
-    .catch(function(error)
+        if(win)
+        {
+            winCondition();
+        }
+    }
+}
+
+// Handles revealing letters, 
+// disabling keys,
+// checking game state on each guess
+function handleGuess($button, letter)
+{
+    let match = revealLetter(letter);
+
+    if(match === false)
     {
-        $word.text(error);
-    });
+        wrongGuesses++;
+
+        $image.attr("src", `../images/state${wrongGuesses}.jpg`);
+        $image.attr("alt", `state${wrongGuesses}.jpg`);
+    }
+
+    disableKey($button);
+    checkGameState();
+    $incorrect.text("Wrong guesses: " + wrongGuesses);
+}
+
+
+// ****************
+// *** END GAME ***
+// ****************
+
+function winCondition()
+{
+    $image.attr("src", "../images/win.jpg");
+    $image.attr("alt", "win.jpg");
+
+    $keyboard.hide();
+    $word.hide();
+    $hint.hide();
+    $hintButton.hide();
+    $incorrect.hide();
+
+    $reset.text("Play Again");
+    $("#instructions p").text("Congratulations! Hammy couldn't steal all your seeds!!");
+}
+
+function loseCondition()
+{
+    $image.attr("src", "../images/lose.jpg");
+    $image.attr("alt", "lose.jpg");
+
+    $keyboard.hide();
+    $word.hide();
+    $hint.hide();
+    $hintButton.hide();
+    $incorrect.hide();
+
+    $reset.text("Play Again");
+    $("#instructions p").text("Oh no!! All your seeds were stolen. Better luck next time. :(");
+}
